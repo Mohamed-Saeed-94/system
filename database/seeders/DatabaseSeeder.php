9d9fe47af6b1f9be99d1d90508ff40236846af44
mohamed-saeed-94 <mohamed.said5010@gmail.com>
2025-12-19 06:08:22 +0300

    وصف التعديل

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,22 @@ class DatabaseSeeder extends Seeder
                 'email_verified_at' => now(),
             ]
         );
-
+        $hrUser = User::firstOrCreate(
+            ['email' => 'hr@hr.com'],
+            [
+                'name' => 'HR User',
+                'password' => bcrypt('Hr@123'),
+                'email_verified_at' => now(),
+            ]
+        );
+        $employeeUser = User::firstOrCreate(
+            ['email' => 'employee@employee.com'],
+            [
+                'name' => 'Employee User',
+                'password' => bcrypt('Employee@123'),
+                'email_verified_at' => now(),
+            ]
+        );
 
     }
 }
