e0403f8f6a61c30fd2df40f4e23f2dd7adbf01d0
mohamed-saeed-94 <mohamed.said5010@gmail.com>
2025-12-19 16:37:53 +0300

    وصف التعديل

--- app/Modules/Core/Models/City.php
+++ app/Modules/Core/Models/City.php
@@ -2,10 +2,12 @@
 
 namespace App\Modules\Core\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
+    use CrudTrait;
+
     protected $fillable = ['name', 'is_active'];
 }
-
