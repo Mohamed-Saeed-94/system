b9f4a1d930a73c9894ae9f3e1d87f91249c2b7b6
mohamed-saeed-94 <mohamed.said5010@gmail.com>
2025-12-19 06:05:25 +0300

    وصف التعديل

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'Admin User',
+                'password' => bcrypt('Admin@123'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+
     }
 }
